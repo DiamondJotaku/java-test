45ad708141c567bed840430753bc47317b1e6be2
DiamondJotaku <98100085+DiamondJotaku@users.noreply.github.com>
2026-05-28 16:09:26 +0200

    Delete demo (2) directory

--- demo (2)/demo/src/main/java/com/example/demo/repository/CategorieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Categorie;
-
-public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-}
